5f228e02f33738c0dac3c3bf21c28f22d1e0a745
ChauTNguyen <ChauTNguyen96@gmail.com>
2016-04-08 19:14:09 -0700

    woops forgot this one

---
+++ Logic.java
@@ -0,0 +1,21 @@
+public class Logic {
+    
+    public int movesCounter;
+    public String userSymbol;
+    View view;
+    
+    public Logic(View view) {
+        movesCounter = 0;
+        this.view = view;
+    }
+    
+    public void setChoice() {
+        if (movesCounter % 2 == 0 )
+            userSymbol = "X";
+        else
+            userSymbol = "O";
+    }
+    
+    public void incrementMovesCounter() { movesCounter++; }
+    
+}
