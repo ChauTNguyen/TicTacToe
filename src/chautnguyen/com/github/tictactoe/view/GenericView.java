8384ac0c8095dcd058877f0c5dd5fb0943de65ba
ChauTNguyen <ChauTNguyen96@gmail.com>
2016-05-03 14:45:56 -0700

    forgot to add this one in

---
+++ src/chautnguyen/com/github/tictactoe/view/GenericView.java
@@ -0,0 +1,28 @@
+package chautnguyen.com.github.tictactoe.view;
+
+import chautnguyen.com.github.tictactoe.model.Field.Symbol;
+
+import javax.swing.JButton;
+
+interface GenericView
+{
+    /**
+     * Changes a field to a user symbol.
+     * 
+     * @param symbol    the symbol of the current player.
+     * @param button    the button that was clicked.
+     */
+    public void setFieldOwner(Symbol owner, JButton button);
+    
+    /**
+     * Informs the user who won.
+     * 
+     * @param symbol    the symbol of the current player (=> winner).
+     */
+    public void informWin(Symbol userSymbol);
+    
+    /**
+     * Informs the user of the tie.
+     */
+    public void informTie();
+}
